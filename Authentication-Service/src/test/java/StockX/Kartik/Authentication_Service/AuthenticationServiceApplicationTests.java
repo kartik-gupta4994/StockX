c1410279ad1f1ae744a46db93d362597e7f9cfb4
kartik-gupta4994 <kartik.gupta1098@gmail.com>
2025-06-14 01:18:35 +0530

    Added Eureka Server and Parent module

--- Authentication-Service/src/test/java/StockX/Kartik/Authentication_Service/AuthenticationServiceApplicationTests.java
+++ Authentication-Service/src/test/java/StockX/Kartik/Authentication_Service/AuthenticationServiceApplicationTests.java
@@ -1,13 +1,3 @@
 package StockX.Kartik.Authentication_Service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AuthenticationServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
